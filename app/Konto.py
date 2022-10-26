08fef4d7202e818eab334a6126c7668f9dd06a2c
Zofia Piesik <piesikzofia09@gmail.com>
2022-10-26 16:53:19 +0200

    Feature_4
    - Added discount code: + 50 zł for new account
    Tests - checking if:
    - code starts with "PROM_" and has 3 next random characters
    - the length is equal 8
    - the saldo is 50 if code is correct

--- app/Konto.py
+++ app/Konto.py
@@ -1,6 +1,11 @@
 class Konto:
-    def __init__(self, imie, nazwisko, pesel):
+    def __init__(self, imie, nazwisko, pesel, kod_rabatowy=None):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
-        self.pesel = pesel  # feature 2
+        self.pesel = pesel  # feature_2
+        self.kod_rabatowy = kod_rabatowy  # fearute_3
+
+        # bonus za kod rabatowy
+        if (self.kod_rabatowy != None):
+            self.saldo += 50
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -1,4 +1,4 @@
-import string
+import re
 import unittest
 
 from ..Konto import Konto
@@ -7,18 +7,36 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        imie = "Dariusz"
-        nazwisko = "Januszewski"
-        pesel = 90030384515
-        pierwsze_konto = Konto(imie, nazwisko, pesel)
-        self.assertEqual(pierwsze_konto.imie, "Dariusz",
-                         "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski",
-                         "Nazwisko nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-        self.assertEqual(pierwsze_konto.pesel, pesel,
-                         "Brak numeru pesel!")  # feature 2
-        self.assertEqual(len(str(pierwsze_konto.pesel)) == 11,
-                         True, "Niepoprawny pesel!")
+        imie01 = "Dariusz"
+        nazwisko01 = "Januszewski"
+        pesel01 = 90030384515
+        kod01 = "PROM_NNE"
+        pierwsze_konto = Konto(imie01, nazwisko01, pesel01, kod01)
 
-    # tutaj proszę dodawać nowe testy
+        self.assertEqual(pierwsze_konto.imie, imie01,
+                         "Imie nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.nazwisko, nazwisko01,
+                         "Nazwisko nie zostało zapisane!")
+
+        # sprawdzanie numeru pesel
+        self.assertEqual(pierwsze_konto.pesel, pesel01,
+                         "Brak numeru pesel!")  # feature 2
+        self.assertEqual(len(str(pierwsze_konto.pesel)),
+                         11, "Niepoprawny pesel!")
+
+        # realizacja kodu rabatowego
+
+        def sprawdzanie_kodu_rabatowego(kod, saldo):
+            if (kod != None):
+                pattern = re.compile(r"PROM_([a-zA-Z0-9]){3}")
+                self.assertTrue(re.match(pattern, kod), "Błędny kod rabatowy")
+                self.assertEqual(len(kod), 8, "Niepoprawna długość kodu.")
+
+                # sprawdzanie czy konto zostało doładowane
+                self.assertEqual(saldo, 50, "Kod nie zadziałał.")
+
+            else:
+                self.assertEqual(saldo, 0, "Saldo nie jest zerowe!")
+
+        sprawdzanie_kodu_rabatowego(
+            pierwsze_konto.kod_rabatowy, pierwsze_konto.saldo)
