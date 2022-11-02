6cf7fb616727949fad6dc56dbd63f6c59d575bf9
Zofia Piesik <piesikzofia09@gmail.com>
2022-11-02 15:38:18 +0100

    update lab02

--- app/Konto.py
+++ app/Konto.py
@@ -1,11 +1,40 @@
+import re
+
+
 class Konto:
     def __init__(self, imie, nazwisko, pesel, kod_rabatowy=None):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
-        self.pesel = pesel  # feature_2
-        self.kod_rabatowy = kod_rabatowy  # fearute_3
+        self.pesel = pesel
 
         # bonus za kod rabatowy
-        if (self.kod_rabatowy != None) and (self.pesel[0:2] > "60") and (self.pesel[4] < "8"):
+
+        codePat = re.compile(r'^PROM_([a-zA-Z0-9]){3}$')
+        codeCorrect = True if re.match(
+            codePat, self.kod_rabatowy) else "Niepoprawny kod!"
+
+        codeRules = (((int(self.pesel[0:2]) in range(61, 100)) and (int(self.pesel[2]) in [0, 1]))
+                     or ((int(self.pesel[0:2]) in range(2, 100)) and (int(self.pesel[2]) in range(2, 7))))
+
+        self.kod_rabatowy = kod_rabatowy if codeRules else "Niespełnione warunki promocji!"
+
+        if codeCorrect and codeRules:
             self.saldo += 50
+
+    def zaksieguj_przelew_wychodzacy(self, kwota):
+        if kwota > self.saldo:
+            return "Nie zaksiegowano przelewu przychodzącego!"
+        else:
+            self.saldo -= kwota
+
+    def zaksieguj_przelew_przychodzacy(self, kwota):
+        self.saldo += kwota
+
+
+class KontoFirmowe(Konto):
+
+    def __init__(self, nazwa_firmy, nip):
+        self.nazwa_firmy = nazwa_firmy
+        self.nip = nip if len(nip) == 10 else "Niepoprwany NIP!"
+        self.saldo = 0
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -6,63 +6,43 @@ from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
 
+    imie = "Dariusz"
+    nazwisko = "Januszewski"
+    pesel = "50030384515"
+    kod = "PROM_NNE"
+
     def test_tworzenie_konta(self):
-
-        # konto seniora
-        imie_01 = "Dariusz"
-        nazwisko_01 = "Januszewski"
-        pesel_01 = "50030384515"
-        kod_01 = "PROM_NNE"
-        konto_01 = Konto(imie_01, nazwisko_01, pesel_01, kod_01)
-
-        # konto normalne
-        imie_02 = "Grażyna"
-        nazwisko_02 = "Januszewska"
-        pesel_02 = "61011225848"
-        kod_02 = "PROM_K26"
-        konto_02 = Konto(imie_02, nazwisko_02, pesel_02, kod_02)
-
-        # sprawdzanie czy informacje zostały zapisane
-        self.assertEqual(konto_01.imie, imie_01,
+        konto = Konto(self.imie, self.nazwisko, self.pesel, self.kod)
+        self.assertEqual(konto.imie, self.imie,
                          "Imie nie zostało zapisane!")
-        self.assertEqual(konto_01.nazwisko, nazwisko_01,
+        self.assertEqual(konto.nazwisko, self.nazwisko,
                          "Nazwisko nie zostało zapisane!")
-        self.assertEqual(konto_01.pesel, pesel_01,
+        self.assertEqual(konto.pesel, self.pesel,
                          "Pesel nie został zapisany!")
+        self.assertEqual(konto.saldo, 0, "Saldo nie jest zerowe.")
 
-        self.assertEqual(konto_02.imie, imie_02,
-                         "Imie nie zostało zapisane!")
-        self.assertEqual(konto_02.nazwisko, nazwisko_02,
-                         "Nazwisko nie zostało zapisane!")
-        self.assertEqual(konto_02.pesel, pesel_02,
-                         "Pesel nie został zapisany!")
+    def test_za_dlugi_pesel(self, konto):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, self.kod)
+        konto.pesel = "05555858488485582756"
+        self.assertEqual(len(konto.pesel), "Niepoprawny pesel!")
 
-        def sprawdzanie_poprawnosci_nr_pesel(konto):
-            self.assertTrue(isinstance(konto.pesel, str),
-                            "Pesel nie może być liczbą!")
-            self.assertEqual(len(konto.pesel),
-                             11, "Niepoprawny pesel!")
+    def test_za_krotki_pesel(self, konto):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, self.kod)
+        konto.pesel = "055558"
+        self.assertEqual(len(konto.pesel), "Niepoprawny pesel!")
 
-        # sprawdzanie peseli dla kont
-        sprawdzanie_poprawnosci_nr_pesel(konto_01)
-        sprawdzanie_poprawnosci_nr_pesel(konto_01)
+    # realizacja kodu rabatowego
+    def test_niepoprawny_kod_rabatowy(self, konto):
+        konto = Konto(self.imie, self. nazwisko, self.pesel)
+        konto.kod_rabatowy = "ccc"
+        self.assertTrue(
+            konto.kod_rabatowy, "Błędny kod rabatowy")
+        self.assertEqual(konto.saldo, 0, "Saldo nie jest zerowe!")
 
-        # realizacja kodu rabatowego
-        def sprawdzanie_kodu_rabatowego(konto):
-            if (konto.kod_rabatowy != None) and (konto.pesel[0:2] > "60") and (konto.pesel[4] < "8"):
-                pattern = re.compile(r'^PROM_([a-zA-Z0-9]){3}$')
-                self.assertTrue(
-                    re.match(pattern, konto.kod_rabatowy), "Błędny kod rabatowy")
-
-                # sprawdzanie czy konto zostało doładowane
-                self.assertEqual(konto.saldo, 50, "Kod nie zadziałał.")
-
-            else:
-                self.assertEqual(konto.saldo, 0, "Saldo nie jest zerowe!")
-
-        # sprawdzanie kodów dla kont
-        sprawdzanie_kodu_rabatowego(konto_01)
-        sprawdzanie_kodu_rabatowego(konto_02)
-
-        # sprawdzenie czy konta na pewno się różnią rabatem
-        self.assertTrue(konto_01.saldo != konto_02.saldo)
+    def test_poprawny_kod_rabatowy(self, konto):
+        konto = Konto(self.imie, self. nazwisko, self.pesel)
+        konto.kod_rabatowy = "PROM_5845"
+        self.assertFalse(
+            konto.kod_rabatowy, "Błędny kod rabatowy")
+        # sprawdzanie czy konto zostało doładowane
+        self.assertEqual(konto.saldo, 50, "Kod nie zadziałał.")
