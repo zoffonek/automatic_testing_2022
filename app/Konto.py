1b937a6b9fefc0559d5ae5db855a069069d585dc
Zofia Piesik <piesikzofia09@gmail.com>
2022-11-19 15:47:39 +0100

    feature 13

--- app/Konto.py
+++ app/Konto.py
@@ -13,23 +13,29 @@ class Konto(object):
         self.oplata_ekspres = 1
         self.historia = []
 
-        # bonus za kod rabatowy
-        def zrealizuj_kod():
-            if kod_rabatowy != None:
-                codePat = re.compile(r'^PROM_([a-zA-Z0-9]){3}$')
-                codeCorrect = True if re.match(
-                    codePat, kod_rabatowy) else False
+        # _____bonus za kod rabatowy _____
 
-                codeRules = (((int(self.pesel[0:2]) in range(61, 100)) and (int(self.pesel[2]) in [0, 1]))
-                             or ((int(self.pesel[0:2]) in range(2, 100)) and (int(self.pesel[2]) in range(2, 7))))
-                if codeCorrect and codeRules:
+        def urodzenie_po_1961():
+            pesel_1961_2000 = ((int(self.pesel[0:2]) in range(
+                61, 100)) and (int(self.pesel[2]) in [0, 1]))
+            pesel_po_2000 = ((int(self.pesel[0:2]) in range(2, 100)) and (
+                int(self.pesel[2]) in range(2, 7)))
+            return (pesel_1961_2000 or pesel_po_2000)
+
+        def poprawny_kod():
+            wzor = re.compile(r'^PROM_([a-zA-Z0-9]){3}$')
+            return True if re.match(wzor, self.kod_rabatowy) else False
+
+        def zrealizuj_kod():
+            if self.kod_rabatowy != None:
+                if poprawny_kod() and urodzenie_po_1961():
                     self.saldo += 50
                 else:
                     self.kod_rabatowy = "Błędny kod rabatowy!"
 
         zrealizuj_kod()
 
-    # przelewy
+    #  ____________________przelewy___________________________
 
     def przelew_wychodzacy(self, kwota):
         if kwota > self.saldo:
@@ -50,16 +56,21 @@ class Konto(object):
             self.saldo -= self.oplata_ekspres
             self.historia.append(-self.oplata_ekspres)
 
-    # kredyt
+    # _________________________kredyt ___________________________________
+
+    def historia_niemniejsza_od_3(self):
+        return len(self.historia) >= 3
+
+    def ostatnie_3_transakcje_przychodzace(self):
+        return (self.historia[-1] > 0 and self.historia[-2] > 0 and self.historia[-3] > 0)
+
+    def suma_5_ostatnich_transakji_wieksza_od_kwoty(self, kwota):
+        return (len(self.historia) >= 5 and sum(self.historia[-5:]) > kwota)
 
     def zaciagnij_kredyt(self, kwota):
-        if len(self.historia) < 3:
-            return False
-        if (self.historia[-1] > 0 and self.historia[-2] > 0 and self.historia[-3] > 0):
-            self.saldo += kwota
-            return True
-
-        if (len(self.historia) >= 5 and sum(self.historia[-5:]) > kwota):
+        if self.historia_niemniejsza_od_3() and \
+                (self.ostatnie_3_transakcje_przychodzace() or
+                 self.suma_5_ostatnich_transakji_wieksza_od_kwoty(kwota)):
             self.saldo += kwota
             return True
         return False
@@ -82,3 +93,18 @@ class KontoFirmowe(Konto):
 
     def przelew_ekspresowy(self, kwota):
         return super().przelew_ekspresowy(kwota)
+
+    # _____________________kredyt_________________________
+
+    def przelew_do_ZUS_min_1(self):
+        return (-1775 in self.historia)
+
+    def min_2x_wieksze_saldo_od_kwoty(self, kwota):
+        return sum(self.historia) >= kwota
+
+    def zaciagnij_kredyt(self, kwota):
+        if self.przelew_do_ZUS_min_1() and \
+                self.min_2x_wieksze_saldo_od_kwoty(kwota):
+            self.saldo += kwota
+            return True
+        return False
--- app/tests/test_historia.py
+++ app/tests/test_historia.py
@@ -11,92 +11,87 @@ class TestHistoria(unittest.TestCase):
     nazwa_firmy = "Januszex sp. z o.o."
     nip = "8461627563"
 
+    def setUp(self):
+        self.konto = Konto(self.imie, self.nazwisko, self.pesel)
+        self.kontoF = KontoFirmowe(self.nazwa_firmy, self.nip)
+
     def test_udana_historia_przychodzace(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.przelew_przychodzacy(50)
-        konto.przelew_przychodzacy(100)
-        konto.przelew_przychodzacy(20)
-        self.assertEqual(konto.historia, [
+        self.konto.przelew_przychodzacy(50)
+        self.konto.przelew_przychodzacy(100)
+        self.konto.przelew_przychodzacy(20)
+        self.assertEqual(self.konto.historia, [
                          50, 100, 20], "Nieudany zapis historii - przychodzące")
 
     def test_nieudana_historia_wychodzace(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.saldo = 150
-        konto.przelew_wychodzacy(50)
-        konto.przelew_wychodzacy(100)
-        konto.przelew_wychodzacy(20)
+        self.konto.saldo = 150
+        self.konto.przelew_wychodzacy(50)
+        self.konto.przelew_wychodzacy(100)
+        self.konto.przelew_wychodzacy(20)
         self.assertEqual(
-            konto.historia, [-50, -100], "Nieudany zapis historii - wychodzące")
+            self.konto.historia, [-50, -100], "Nieudany zapis historii - wychodzące")
 
     def test_udana_historia_wychodzace(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.saldo = 60000
-        konto.przelew_wychodzacy(35)
-        konto.przelew_wychodzacy(110)
-        konto.przelew_wychodzacy(200)
+
+        self.konto.saldo = 60000
+        self.konto.przelew_wychodzacy(35)
+        self.konto.przelew_wychodzacy(110)
+        self.konto.przelew_wychodzacy(200)
         self.assertEqual(
-            konto.historia, [-35, -110, -200], "Nieudany zapis historii - wychodzące")
+            self.konto.historia, [-35, -110, -200], "Nieudany zapis historii - wychodzące")
 
     def test_udana_historia_ekspres(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.saldo = 60000
-        konto.przelew_ekspresowy(35)
-        konto.przelew_ekspresowy(110)
-        konto.przelew_ekspresowy(200)
+        self.konto.saldo = 60000
+        self.konto.przelew_ekspresowy(35)
+        self.konto.przelew_ekspresowy(110)
+        self.konto.przelew_ekspresowy(200)
         self.assertEqual(
-            konto.historia, [-35, -1, -110, -1, -200, -1], "Nieudany zapis historii - wychodzące (ekspres)")
+            self.konto.historia, [-35, -1, -110, -1, -200, -1], "Nieudany zapis historii - wychodzące (ekspres)")
 
     def test_nieudana_historia_ekspres(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.saldo = 40
-        konto.przelew_ekspresowy(35)
-        konto.przelew_ekspresowy(5)
-        konto.przelew_ekspresowy(200)
+        self.konto.saldo = 40
+        self.konto.przelew_ekspresowy(35)
+        self.konto.przelew_ekspresowy(5)
+        self.konto.przelew_ekspresowy(200)
         self.assertEqual(
-            konto.historia, [-35, -1], "Nieudany zapis historii - wychodzące (ekspres)")
+            self.konto.historia, [-35, -1], "Nieudany zapis historii - wychodzące (ekspres)")
 
     # przelewy firmowe
 
     def test_udana_historia_przychodzace_firma(self):
-        konto = KontoFirmowe(self.nazwa_firmy, self.nip)
-        konto.przelew_przychodzacy(50)
-        konto.przelew_przychodzacy(100)
-        konto.przelew_przychodzacy(20)
-        self.assertEqual(konto.historia, [
+        self.kontoF.przelew_przychodzacy(50)
+        self.kontoF.przelew_przychodzacy(100)
+        self.kontoF.przelew_przychodzacy(20)
+        self.assertEqual(self.kontoF.historia, [
                          50, 100, 20], "Nieudany zapis historii - przychodzące, firma")
 
     def test_nieudana_historia_wychodzace_firma(self):
-        konto = KontoFirmowe(self.nazwa_firmy, self.nip)
-        konto.saldo = 150
-        konto.przelew_wychodzacy(50)
-        konto.przelew_wychodzacy(100)
-        konto.przelew_wychodzacy(20)
+        self.kontoF.saldo = 150
+        self.kontoF.przelew_wychodzacy(50)
+        self.kontoF.przelew_wychodzacy(100)
+        self.kontoF.przelew_wychodzacy(20)
         self.assertEqual(
-            konto.historia, [-50, -100], "Nieudany zapis historii - wychodzące firma")
+            self.kontoF.historia, [-50, -100], "Nieudany zapis historii - wychodzące firma")
 
     def test_udana_historia_wychodzace_firma(self):
-        konto = KontoFirmowe(self.nazwa_firmy, self.nip)
-        konto.saldo = 60000
-        konto.przelew_wychodzacy(35)
-        konto.przelew_wychodzacy(110)
-        konto.przelew_wychodzacy(200)
+        self.kontoF.saldo = 60000
+        self.kontoF.przelew_wychodzacy(35)
+        self.kontoF.przelew_wychodzacy(110)
+        self.kontoF.przelew_wychodzacy(200)
         self.assertEqual(
-            konto.historia, [-35, -110, -200], "Nieudany zapis historii - wychodzące firma")
+            self.kontoF.historia, [-35, -110, -200], "Nieudany zapis historii - wychodzące firma")
 
     def test_udana_historia_ekspres_firma(self):
-        konto = KontoFirmowe(self.nazwa_firmy, self.nip)
-        konto.saldo = 60000
-        konto.przelew_ekspresowy(35)
-        konto.przelew_ekspresowy(110)
-        konto.przelew_ekspresowy(200)
+        self.kontoF.saldo = 60000
+        self.kontoF.przelew_ekspresowy(35)
+        self.kontoF.przelew_ekspresowy(110)
+        self.kontoF.przelew_ekspresowy(200)
         self.assertEqual(
-            konto.historia, [-35, -5, -110, -5, -200, -5], "Nieudany zapis historii - wychodzące (ekspres - firma)")
+            self.kontoF.historia, [-35, -5, -110, -5, -200, -5], "Nieudany zapis historii - wychodzące (ekspres - firma)")
 
     def test_nieudana_historia_ekspres_firma(self):
-        konto = KontoFirmowe(self.nazwa_firmy, self.nip)
-        konto.saldo = 40
-        konto.przelew_ekspresowy(35)
-        konto.przelew_ekspresowy(5)
-        konto.przelew_ekspresowy(200)
+        self.kontoF.saldo = 40
+        self.kontoF.przelew_ekspresowy(35)
+        self.kontoF.przelew_ekspresowy(5)
+        self.kontoF.przelew_ekspresowy(200)
         self.assertEqual(
-            konto.historia, [-35, -5], "Nieudany zapis historii - wychodzące (ekspres - firma)")
+            self.kontoF.historia, [-35, -5], "Nieudany zapis historii - wychodzące (ekspres - firma)")
